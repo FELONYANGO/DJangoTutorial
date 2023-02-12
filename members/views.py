091e898eeedec3277856ac1aa27d99cfd47b0ca9
FELONYANGO <felixonyango951@gmail.com>
2023-02-12 04:05:59 +0300

    updated views.py for base folder

--- members/views.py
+++ members/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Members
 
 # Create your views here.
 def  members(request):
-    template=loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    template=loader.get_template('base.html')
+    member=Members.objects.all().values()
+    context={
+        'member':member
+    }
+    return HttpResponse(template.render(context,request))
+    
