d69c48ee01747d7e8bbf728c129f41f0562c1a63
FELONYANGO <felixonyango951@gmail.com>
2023-02-13 23:25:32 +0300

    added simple landing page

--- members/views.py
+++ members/views.py
@@ -19,3 +19,7 @@ def details(request,id):
         'myMembers':myMembers,
     }
     return HttpResponse(template.render(context,request))
+
+def main(request):
+    template=loader.get_template('main.html')
+    return HttpResponse(template.render())
