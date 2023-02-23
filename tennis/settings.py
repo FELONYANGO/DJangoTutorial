50ee92a26e220ff195301432f6ff459d1245d8c3
FELONYANGO <felixonyango951@gmail.com>
2023-02-24 02:22:14 +0300

    checking the bootstrap error on render

--- tennis/settings.py
+++ tennis/settings.py
@@ -37,6 +37,7 @@ ADMIN_MEDIA_PREFIX = '/admin-media/'
 # Application definition
 
 INSTALLED_APPS = [
+    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -44,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'members',
-    'bootstrap5',
+    
 ]
 
 MIDDLEWARE = [
