0d82c40f96b49a52c2f7e11a8779893e6c9ff557
FELONYANGO <felixonyango951@gmail.com>
2023-02-23 18:17:24 +0300

    admin

--- members/admin.py
+++ members/admin.py
@@ -4,4 +4,5 @@ from.models import Members
 # Register your models here.
 class MemberAdmin(admin.ModelAdmin):
     list_display = ('firstname', 'lastname','phone','joined_date')
+    prepopulated_fields={"slug": ('firstname','lastname')}
 admin.site.register(Members,MemberAdmin)
--- members/models.py
+++ members/models.py
@@ -7,6 +7,7 @@ class Members(models.Model):
     lastname = models.CharField(max_length=255)
     phone=models.IntegerField(null=True)
     joined_date=models.DateTimeField(null=True)
+    slug=models.SlugField(default="", null=True)
 
     def __str__(self):
        return f"{self.firstname} {self.lastname}"
